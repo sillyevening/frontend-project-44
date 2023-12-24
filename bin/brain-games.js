1b9124556b34f387c1f80778651272d708d3fe68
angelina ivanova <chernika.kostya@mail.ru>
2023-12-24 22:36:25 +0300

    fix brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
-import {name} from '../src/cli.js';
-
+import {readlineSync} from '../src/cli.js';
+console.log('Welcome to the Brain Games!');
+console.log('May i have your name?');
+let name = readlineSync.question('Your name: ')
+console.log('Hello,'+name+'!');
 
 
--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,4 @@
 import readlineSync from '../readline-sync/lib/readline-sync.js';
-console.log('Welcome to the Brain Games!');
-console.log('May i have your name?');
-const name = readlineSync.question('Your name: ')
-console.log('Hello,'+name+'!');
 
-export {name} ;
+
+export {readlineSync} ;
